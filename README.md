6235320e2276a19a8c758380131bb57897e0a981
Payton Swick <payton@foolord.com>
2016-03-08 17:07:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# MarkupFrame
+
+A [React](https://facebook.github.io/react/) component to display raw html markup inside an iframe.
+
+Sometimes you need to display html inside an iframe, but not it's not at a URL.  Perhaps you got the markup from an API endpoint or generated it yourself.  Maybe the markup is from a different domain and you need to be able to manipulate its DOM without cross-origin errors. For all these reasons, you can use MarkupFrame.
+
+## Usage
+
+```javascript
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarkupFrame from 'markup-frame';
+import getMarkupFromAPI from './my-api-lib';
+
+const FrameWrapper = React.createClass( {
+	onFrameLoad( frameDocument ) {
+		frameDocument.querySelectorAll( '.article-title' ).html( 'MarkupFrame is Great!' );
+	},
+
+	render() {
+		return (
+			<MarkupFrame markup={ getMarkupFromAPI() } onLoad={ this.onFrameLoad } />
+		);
+	}
+} );
+
+ReactDOM.render(
+	<FrameWrapper>,
+	document.getElementById( '.content' )
+);
+```
+
+## Props
+
+* `markup`: The markup to display in the preview.
+* `onClick`: (Optional) A function which will be called when any DOM element is clicked. Will be passed the raw event.
+* `onLoad`: (Optional) A function which will be called when the DOM is loaded. Will be passed a reference to the DOM.
+* `onClickLink`: (Optional) A function which will be called when a link in the DOM is clicked. Will be passed the destination URL.
---
+++ src/index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import debugFactory from 'debug';
+
+const noop = () => null;
+const debug = debugFactory( 'calypso:site-preview' );
+
+export default React.createClass( {
+	displayName: 'MarkupFrame',
+
+	propTypes: {
+		// The markup to display in the preview.
+		markup: React.PropTypes.string.isRequired,
+		// The handler to call when an element is clicked. Will be passed the event.
+		onClick: React.PropTypes.func,
+		// The handler to call when a link is clicked. Will be passed the target URL.
+		onClickLink: React.PropTypes.func,
+		// The function to call when the DOM is loaded. Will be passed the DOM.
+		onLoad: React.PropTypes.func,
+	},
+
+	getDefaultProps() {
+		return {
+			markup: '',
+			onClick: noop,
+			onClickLink: noop,
+			onLoad: noop,
+		};
+	},
+
+	componentDidMount() {
+		this.updateFrameContent( this.props.markup );
+	},
+
+	componentDidUpdate() {
+		this.updateFrameContent( this.props.markup );
+	},
+
+	shouldComponentUpdate( nextProps ) {
+		if ( this.hasMarkupChanged( this.props.markup, nextProps.markup ) ) {
+			debug( 'markup has changed; re-rendering preview' );
+			return true;
+		}
+		return false;
+	},
+
+	hasMarkupChanged( oldMarkup, newMarkup ) {
+		return ( oldMarkup !== newMarkup );
+	},
+
+	updateFrameContent( content ) {
+		debug( 'adding content to iframe', content.length );
+		this.iframe.addEventListener( 'load', this.finishPreviewLoad );
+		this.iframe.contentDocument.open();
+		this.iframe.contentDocument.write( content );
+		this.iframe.contentDocument.close();
+	},
+
+	finishPreviewLoad() {
+		this.props.onLoad( this.iframe.contentDocument );
+		const domLinks = Array.prototype.slice.call( this.iframe.contentDocument.querySelectorAll( 'a' ) );
+		debug( `disabling ${domLinks.length} links in preview` );
+		domLinks.map( this.disableLink );
+		this.iframe.contentDocument.body.onclick = this.handleClick;
+	},
+
+	disableLink( element ) {
+		const url = element.href;
+		element.href = '#';
+		element.onClick = () => this.handleClickLink( url );
+	},
+
+	handleClickLink( url ) {
+		debug( 'click on link detected for url', url );
+		this.props.onClickLink( url );
+	},
+
+	handleClick( event ) {
+		debug( 'click detected for element', event.target );
+		this.props.onClick( event );
+	},
+
+	saveIframe( el ) {
+		this.iframe = el;
+	},
+
+	render() {
+		return (
+			<div className="markup-frame">
+				<iframe
+					className="markup-frame-iframe"
+					ref={ this.saveIframe }
+				/>
+			</div>
+		);
+	}
+} );
