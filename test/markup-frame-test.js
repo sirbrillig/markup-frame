e5c01d6b96b1aec411017bf20069d6706623ff60
Payton Swick <payton@foolord.com>
2016-03-08 19:28:26 -0500

    Update tests to cover most common cases
    They don't work, of course, because (I think) some mix of React, jsdom, and
    iframes don't work correctly together.

--- test/markup-frame-test.js
+++ test/markup-frame-test.js
@@ -3,13 +3,20 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import MarkupFrame from '../src';
 
-describe( '<MarkupFrame />', () => {
-	it( 'renders an iframe', () => {
+describe( '<MarkupFrame />', function() {
+	it( 'renders an iframe', function() {
 		const wrapper = mount( <MarkupFrame markup="" /> );
-		expect( wrapper.some( 'iframe' ) ).to.equal( true );
+		expect( wrapper.find( 'iframe' ) ).to.have.length( 1 );
 	} );
 
-	it( 'injects props.markup into the iframe' );
+	it( 'injects props.markup into the iframe', function() {
+		const wrapper = mount( <MarkupFrame markup="<h1>Hello World</h1>" /> );
+		expect( wrapper.find( 'iframe' ).get( 0 ).innerHTML ).to.eql( '<h1>Hello World</h1>' )
+	} );
 
-	it( 'calls props.onLoad with the iframe document' );
+	it( 'calls props.onLoad with the iframe document', function() {
+		const onLoad = doc => doc.querySelector( 'h1' ).innerHTML = 'greetings';
+		const wrapper = mount( <MarkupFrame markup="<h1>Hello World</h1>" onLoad={ onLoad } /> );
+		expect( wrapper.find( 'iframe' ).get( 0 ).innerHTML ).to.eql( '<h1>greetings</h1>' )
+	} );
 } );
