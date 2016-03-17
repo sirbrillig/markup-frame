f933dfeb244563a5d567c5baae73629ffb3dde1f
Payton Swick <payton@foolord.com>
2016-03-17 11:18:21 -0400

    Update test to use ReactDOM
    See
    http://stackoverflow.com/questions/36024664/why-does-rendering-an-iframe-into-jsdom-with-reacttestutils-not-provide-a-conten/36048152#36048152

--- test/markup-frame-test.js
+++ test/markup-frame-test.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
 import MarkupFrame from '../src';
+
+function mount( component ) {
+	const contentArea = window.document.querySelector( '#content' );
+	ReactDOM.render( component, contentArea );
+}
 
 describe( '<MarkupFrame />', function() {
 	it( 'renders an iframe', function() {
-		const wrapper = mount( <MarkupFrame markup="" /> );
-		expect( wrapper.find( 'iframe' ) ).to.have.length( 1 );
+		mount( <MarkupFrame markup="" /> );
+		const iframes = window.document.querySelectorAll( 'iframe' );
+		expect( iframes ).to.have.length( 1 );
 	} );
 
 	it( 'injects props.markup into the iframe', function() {
-		const wrapper = mount( <MarkupFrame markup="<h1>Hello World</h1>" /> );
-		expect( wrapper.find( 'iframe' ).get( 0 ).innerHTML ).to.eql( '<h1>Hello World</h1>' )
+		mount( <MarkupFrame markup="<h1>Hello World</h1>" /> );
+		const iframe = window.document.querySelector( 'iframe' );
+		expect( iframe.contentWindow.document.body.innerHTML ).to.eql( '<h1>Hello World</h1>' )
 	} );
 
-	it( 'calls props.onLoad with the iframe document', function() {
+	it( 'calls props.onLoad with the iframe document', function( done ) {
 		const onLoad = doc => doc.querySelector( 'h1' ).innerHTML = 'greetings';
-		const wrapper = mount( <MarkupFrame markup="<h1>Hello World</h1>" onLoad={ onLoad } /> );
-		expect( wrapper.find( 'iframe' ).get( 0 ).innerHTML ).to.eql( '<h1>greetings</h1>' )
+		mount( <MarkupFrame markup="<h1>Hello World</h1>" onLoad={ onLoad } /> );
+		setTimeout( () => {
+			const iframe = window.document.querySelector( 'iframe' );
+			expect( iframe.contentWindow.document.body.innerHTML ).to.eql( '<h1>greetings</h1>' );
+			done();
+		}, 200 );
 	} );
 } );
--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,7 @@ var jsdom = require('jsdom').jsdom;
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
+global.document = jsdom('<div id="content"></div>');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
